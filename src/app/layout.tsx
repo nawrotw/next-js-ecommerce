70b200ab8ac20deff47ef8e3dd9dd835a18acd7e
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-06-19 08:20:52 +0200

    AdminPage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,16 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+{/*
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
       enableSystem
       disableTransitionOnChange
     >
+*/}
       {children}
-    </ThemeProvider>
+    {/*</ThemeProvider>*/}
     </body>
     </html>
   );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { ReactNode, ComponentProps } from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export function Nav({
+  children,
+}: Readonly<{ children: ReactNode; }>) {
+  return <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+    {children}
+  </nav>
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>,'className'>) {
+  const pathName = usePathname();
+  const isCurrentPath = pathName === props.href;
+  return <Link {...props} className={cn("p-4 " +
+    "hover:bg-secondary hover:text-secondary-foreground" +
+    "focus-visible:bg-secondary focus-visible:text-secondary-foreground" +
+    isCurrentPath && "bg-background text-foreground"
+  )}></Link>
+}
