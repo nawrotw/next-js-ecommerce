f692242abcbba926b012cce669d855c21f73e2ec
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-08 11:15:52 +0200

    env fix

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
-    DIRECT_DATABASE_URL: z.string().min(1),
     NEXTAUTH_SECRET: z.string().min(1),
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
   },
